1649420460b30e5de7e4c0bb0c80d8ad636636d2
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-05-15 23:24:06 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,65 +1,27 @@
-const { startBtn, stopBtn, body, resetBtn } = refs;
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('button[data-action = "start"]'),
+  btnStop: document.querySelector('button[data-action = "stop"]'),
+};
 
-const INTERVAL_TIME = 1000;
+const INTERVAL_DELAY = 1000;
 let intervalId = null;
 
-// - Вешаем слушателей
-startBtn.addEventListener('click', startBtnHandler);
-stopBtn.addEventListener('click', stopBtnHandler);
-resetBtn.addEventListener('click', resetBtnColorHandler);
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
 
-// - Генерации рамдомного цвета
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// - Вешает рандомный цвет на body
-function getBodyColor() {
-  return (body.style.backgroundColor = getRandomHexColor());
+function changeColor() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DELAY);
+  refs.btnStart.disabled = true;
 }
 
-// - Обработчик кнопки Старт
-function startBtnHandler({ target }) {
-  if (target.dataset.action.start) {
-    return;
-  }
-  console.log(target);
-
-  if (!target.disabled) {
-    target.disabled = true;
-
-    intervalId = setInterval(getBodyColor, INTERVAL_TIME);
-
-    resetBtn.classList.remove('is_active');
-    body.classList.remove('body_switcher');
-  }
-}
-
-// - Обработчик кнопки Стоп
-function stopBtnHandler({ target }) {
-  if (target.dataset.action.stop) {
-    return;
-  }
-  console.log(target);
-
+function onBtnStop() {
   clearInterval(intervalId);
-
-  if (startBtn.disabled) {
-    startBtn.disabled = false;
-    resetBtn.classList.add('is_active');
-  }
-}
-
-// - Сброс инлайн цвета
-function resetBtnColorHandler({ target }) {
-  if (target.dataset.action.reset) {
-    return;
-  }
-  console.log(target);
-
-  if (!body.classList.contains('body_switcher')) {
-    body.classList.add('body_switcher');
-    resetBtn.classList.remove('is_active');
-  }
-  return (body.style.backgroundColor = '');
+  refs.btnStart.disabled = false;
 }
