e03609da5947933ca74536ac2a81a8fe9f1994cb
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-05-19 00:16:30 +0300

    add task 1 pravki

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,32 @@
 const refs = {
+  bodyElement: document.querySelector('body'),
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
 };
 let interval = null;
+refs.stopBtn.disabled = true;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-function getBodyColor() {
-  document.body.style.backgroundColor = getRandomHexColor();
-}
 
-refs.startBtn.addEventListener('click', onClickIntervalStart);
-function onClickIntervalStart() {
-  interval = setInterval(() => {
-    getBodyColor();
-  }, 1000);
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  getBodyColor();
+  interval = setInterval(getBodyColor, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
 }
 
 refs.stopBtn.addEventListener('click', onStopBtnClick);
+
 function onStopBtnClick() {
   clearInterval(interval);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function getBodyColor() {
+  refs.bodyElement.style.backgroundColor = getRandomHexColor();
 }
